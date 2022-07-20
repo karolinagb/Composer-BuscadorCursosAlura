e7ba295d6f844c5b2944c930942e008be12f63b3
x <karolina_bp@hotmail.com>
2022-07-20 15:55:15 -0300

    Usando teste no ambiente de desenvolvimento

---
+++ tests/TestBuscadorDeCursos.php
@@ -0,0 +1,60 @@
+<?php
+
+use Psr\Http\Message\StreamInterface;
+use GuzzleHttp\Psr7\Stream;
+use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeCursos\Buscador;
+use PHPUnit\Framework\TestCase;
+
+class TestBuscadorDeCursos extends TestCase
+{
+    private $httpClientMock;
+    private $url = 'url-teste';
+
+    protected function setUp(): void
+    {
+        $html = <<<FIM
+        <html>
+            <body>
+                <span class="card-curso__nome">Curso Teste 1</span>
+                <span class="card-curso__nome">Curso Teste 2</span>
+                <span class="card-curso__nome">Curso Teste 3</span>
+            </body>
+        </html>
+        FIM;
+
+        $stream = $this->createMock(Stream::class);
+        $stream
+            ->expects($this->once())
+            ->method('__toString')
+            ->willReturn($html);
+
+        $response = $this->createMock(ResponseInterface::class);
+        $response
+            ->expects($this->once())
+            ->method('getBody')
+            ->willReturn($stream);
+
+        $httpClient = $this
+            ->createMock(ClientInterface::class);
+        $httpClient
+            ->expects($this->once())
+            ->method('request')
+            ->with('GET', $this->url)
+            ->willReturn($response);
+
+        $this->httpClientMock = $httpClient;
+    }
+
+    public function testBuscadorDeveRetornarCursos()
+    {
+        $crawler = new Crawler();
+        $buscador = new Buscador($this->httpClientMock, $crawler);
+        $cursos = $buscador->buscar($this->url);
+
+        $this->assertCount(3, $cursos);
+        $this->assertEquals('Curso Teste 1', $cursos[0]);
+        $this->assertEquals('Curso Teste 2', $cursos[1]);
+        $this->assertEquals('Curso Teste 3', $cursos[2]);
+    }
+}
